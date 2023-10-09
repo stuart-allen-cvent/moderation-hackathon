3e891f18cea4c75fa0efdb5f1a8e5104f84163ce
Stuart Allen <Stuart.Allen@cvent.com>
2023-10-09 16:37:29 -0700

    Working server version

---
+++ server.js
@@ -0,0 +1,23 @@
+const Express = require("express");
+const cors = require('cors');  
+const bodyParser = require('body-parser');  
+const data = require('./data');  
+
+const app = Express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/store', (req, res) => {
+  const { labels } = req.body
+  data.labels = labels
+  console.log(labels)
+  res.send('OK')
+})
+
+app.get('/retrieve', (req, res) => {  
+  res.send({ labels: data.labels });  
+});  
+
+app.listen(3001, () => {  
+  console.log('Server running on port 3001');  
+});  
